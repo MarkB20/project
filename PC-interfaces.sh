7db8ad43374b772007f1e9e92056b6745b855e58
markb20 <w.markbutler2001@gmail.com>
2024-04-14 20:33:33 +0100

    ping working

--- PC-interfaces.sh
+++ PC-interfaces.sh
@@ -2,18 +2,18 @@
 
 # Configure PC1
 sudo docker exec clab-project-PC1 ip link set eth1 up
-sudo docker exec clab-project-PC1 ip addr add 192.168.1.2/24 dev eth1
-sudo docker exec clab-project-PC1 ip route add 192.168.0.0/16 via 192.168.1.1 dev eth1
-sudo docker exec clab-project-PC1 ip route add 10.10.10.0/24 via 192.168.1.1 dev eth1
+sudo docker exec clab-project-PC1 ip addr add 192.168.4.2/24 dev eth1
+sudo docker exec clab-project-PC1 ip route add 192.168.0.0/16 via 192.168.4.1 dev eth1
+sudo docker exec clab-project-PC1 ip route add 10.10.10.0/24 via 192.168.4.1 dev eth1
 
 # Configure PC2
 sudo docker exec clab-project-PC2 ip link set eth1 up
-sudo docker exec clab-project-PC2 ip addr add 192.168.2.2/24 dev eth1
-sudo docker exec clab-project-PC2 ip route add 192.168.0.0/16 via 192.168.2.1 dev eth1
-sudo docker exec clab-project-PC2 ip route add 10.10.10.0/24 via 192.168.2.1 dev eth1
+sudo docker exec clab-project-PC2 ip addr add 192.168.5.2/24 dev eth1
+sudo docker exec clab-project-PC2 ip route add 192.168.0.0/16 via 192.168.5.1 dev eth1
+sudo docker exec clab-project-PC2 ip route add 10.10.10.0/24 via 192.168.5.1 dev eth1
 
 # Configure PC3
 sudo docker exec clab-project-PC3 ip link set eth1 up
-sudo docker exec clab-project-PC3 ip addr add 192.168.3.2/24 dev eth1
-sudo docker exec clab-project-PC3 ip route add 192.168.0.0/16 via 192.168.3.1 dev eth1
-sudo docker exec clab-project-PC3 ip route add 10.10.10.0/24 via 192.168.3.1 dev eth1
+sudo docker exec clab-project-PC3 ip addr add 192.168.6.1/24 dev eth1
+sudo docker exec clab-project-PC3 ip route add 192.168.0.0/16 via 192.168.6.1 dev eth1
+sudo docker exec clab-project-PC3 ip route add 10.10.10.0/24 via 192.168.6.1 dev eth1
