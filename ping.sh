9b4de1f5ce2197b9259e4b56f65ba19c3c121654
markb20 <w.markbutler2001@gmail.com>
2024-04-15 10:05:10 +0100

    changed ping to 0/1

--- ping.sh
+++ ping.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 echo "Testing connection between PC1 and Router1..."
-sudo docker exec clab-project-PC1 ping -c1 192.168.1.1
-echo "Connection test between PC1 and Router1 completed.
-
-
-"
+if sudo docker exec clab-project-PC1 ping -c1 192.168.1.1 >/dev/null; then
+    echo "Connection test between PC1 and Router1: Success"
+else
+    echo "Connection test between PC1 and Router1: Failed"
+fi
+echo
 
 echo "Testing connection between PC2 and Router2..."
-sudo docker exec clab-project-PC2 ping -c1 192.168.5.1
-echo "Connection test between PC2 and Router2 completed.
-
-
-"
+if sudo docker exec clab-project-PC2 ping -c1 192.168.5.1 >/dev/null; then
+    echo "Connection test between PC2 and Router2: Success"
+else
+    echo "Connection test between PC2 and Router2: Failed"
+fi
+echo
 
 echo "Testing connection between PC3 and Router3..."
-sudo docker exec clab-project-PC3 ping -c1 192.168.6.1
-echo "Connection test between PC3 and Router3 completed.
-
-
-"
+if sudo docker exec clab-project-PC3 ping -c1 192.168.6.1 >/dev/null; then
+    echo "Connection test between PC3 and Router3: Success"
+else
+    echo "Connection test between PC3 and Router3: Failed"
+fi
+echo
