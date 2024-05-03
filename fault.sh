2b4dacfdce179208f7c7252d4ccd15b2742ae7f8
markb20 <w.markbutler2001@gmail.com>
2024-05-03 19:32:38 +0100

    edited fault echo

--- fault.sh
+++ fault.sh
@@ -4,7 +4,7 @@ echo "testing fault injection"
 echo "testing before fault injection"
 sudo docker exec clab-project-PC3 traceroute 192.168.5.2
 
-echo "setting router1 eth2 connection to router4 down"
+echo "setting router2 eth1 connection to routerHub down"
 sudo docker exec -d clab-project-router2 ip link set dev eth1 down
 
 echo "testing after fault injection"
